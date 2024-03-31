a08d220fb6d80da4674c8a2fc33ecc4af58ae9bd
Anna <annasukharieva0@gmail.com>
2024-03-31 02:15:51 +0200

    first commit

---
+++ public/form/index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Відправлення форми</title>
+  </head>
+  <body>
+    <form action="/form/h.php" method="post">
+      Логін:  <input type="text" name="login"><br>
+      Пароль: <input type="password" name="pass"><br>
+      <input type="submit" value="Відправити">
+    </form>
+  </body>
+</html>
